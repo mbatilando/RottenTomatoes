d1c9500d74573b17c7a37602ca33274a4ba6d2ab
Mari Batilando <mari.batilando@gmail.com>
2015-02-03 19:24:57 -0800

    Add MovieTableViewCell

---
+++ RottenTomatoes/MovieTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieTitleCell.swift
+//  RottenTomatoes
+//
+//  Created by Mari Batilando on 2/3/15.
+//  Copyright (c) 2015 Mari Batilando. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieTitleLabel: UILabel!
+}
--- RottenTomatoes/ViewController.swift
+++ RottenTomatoes/ViewController.swift
@@ -8,15 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UITableViewController {
 
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let YourApiKey = "esxztx7fqksg7psa53gd3wd5"
+        let RottenTomatoesURLString = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=\(YourApiKey)"
+        let request = NSMutableURLRequest(URL: NSURL(string: RottenTomatoesURLString)!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler:{ (response, data, error) in
+            var errorValue: NSError? = nil
+            let dictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &errorValue) as NSDictionary
+        })
+    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
 
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
-        cell.textLabel?.text = "\(indexPath.row)"
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("com.MariBatilando.cell") as MovieTableViewCell
+        cell.movieTitleLabel.text = "Row \(indexPath.row)"
         return cell
     }
     
